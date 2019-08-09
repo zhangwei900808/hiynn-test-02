1adf04f3f7580c46270a83dc1f5dd3601dfb857a
张威 <zw900808@gmail.com>
2019-08-09 17:34:08 +0800

    初始化前端框架文件

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Spin } from "antd";
+// import { PersistGate } from "redux-persist/es/integration/react";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { Route, Switch } from "react-router";
+import { ConnectedRouter } from "connected-react-router";
+import configureStore, { history } from "./redux/store";
 
-function App() {
+import Login from "./pages/login/Login";
+const { store, persistor } = configureStore({});
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={<Spin />} persistor={persistor}>
+        <ConnectedRouter history={history}>
+          <>
+            <Switch>
+              <Route exact path="/" component={Login} />
+            </Switch>
+          </>
+        </ConnectedRouter>
+      </PersistGate>
+    </Provider>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./assets/css/index.css";
+import "./assets/css/base.css";
+import * as serviceWorker from "./serviceWorker";
+import { ConfigProvider } from "antd";
+import zhCN from "antd/es/locale/zh_CN";
+ReactDOM.render(
+  <ConfigProvider locale={zhCN}>
+    <App />
+  </ConfigProvider>,
+  document.getElementById("root")
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
